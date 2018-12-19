11b026515412d8979828bef6012d012333b28a0f
Autumn-Martin <autumnmartin.dev@gmail.com>
2018-12-19 15:28:45 -0700

    Retrieve twitter data

--- teller/views.py
+++ teller/views.py
@@ -3,6 +3,9 @@ from django.http import JsonResponse
 import requests
 
 def home(request):
-    response = requests.get('https://guarded-reef-25579.herokuapp.com/api/v1/assets')
+    query = 'bitcoin'
+    url = f'https://api.twitter.com/1.1/search/tweets.json?q={query}&lang=en'
+    headers = {'authorization': 'Bearer AAAAAAAAAAAAAAAAAAAAAISA9AAAAAAAW5acT9%2BlGGBQc%2BEslGwUlYjEz8o%3Dog6Fe533FdcoaLlcikecwqRoQMrt9kVIDrbeB5fxktdv0UpCvC'}
+    response = requests.get(url, headers=headers)
     data = response.json()
     return JsonResponse(data, safe=False)
