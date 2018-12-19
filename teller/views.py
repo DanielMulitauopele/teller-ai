59255a36dd852e4b8ef00d8b5d5bba93f9cd7e05
Daniel Mulitauopele <daniel.mulitauopele@gmail.com>
2018-12-19 10:21:12 -0700

    Working on api call

--- teller/views.py
+++ teller/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
+from django.http import JsonResponse
+import requests
 
-from django.http import HttpResponse
-
-def index(request):
-    return HttpResponse("Hello World, you're at the teller index.")
+def home(request):
+    response = requests.get('https://guarded-reef-25579.herokuapp.com/api/v1/assets')
+    data = response.json()
+    return JsonResponse(data, safe=False)
