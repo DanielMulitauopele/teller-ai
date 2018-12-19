6f41fbe6f08bead9eb2a72b2ea99e74aa0d08ac9
Daniel Mulitauopele <daniel.mulitauopele@gmail.com>
2018-12-19 12:53:24 -0700

    Pushing up

--- teller/views.py
+++ teller/views.py
@@ -5,4 +5,5 @@ import requests
 def home(request):
     response = requests.get('https://guarded-reef-25579.herokuapp.com/api/v1/assets')
     data = response.json()
+
     return JsonResponse(data, safe=False)
