0344c1d5a58a819444402830eb64023d5e364325
Autumn-Martin <autumnmartin.dev@gmail.com>
2018-12-29 17:59:25 -0700

    Add Daniel's work on Watson tone analysis

--- teller/views.py
+++ teller/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.http import JsonResponse
+from watson_developer_cloud import ToneAnalyzerV3
 import requests
 import config
 
@@ -11,7 +12,25 @@ def home(request):
     data = response.json()
     statuses = data['statuses']
     tweets = []
-    for status_info in statuses:
-        tweets.append(status_info['text'])
 
-    return JsonResponse(tweets, safe=False)
+    tone_analyzer = ToneAnalyzerV3(
+        version='2017-09-21',
+        iam_apikey='Si1t19DXhzGFq3wgS2rSwvyncuQ6fzX8jwdoe28qWW1s',
+        url='https://gateway.watsonplatform.net/tone-analyzer/api'
+    )
+
+    text = 'I am so in love with the Watson API, although it was a long and arduous process getting this thing established. I wish the documentation was more nuanced, especially in regards to junior developers. Its frustrating slogging through unhelpful docs.'
+
+    tone_analysis = tone_analyzer.tone(
+        {'text': text},
+        'application/json'
+    ).get_result()
+    return JsonResponse(tone_analysis, safe=False)
+
+
+
+
+    # for status_info in statuses:
+    #     tweets.append(status_info['text'])
+    #
+    # return JsonResponse(tweets, safe=False)
