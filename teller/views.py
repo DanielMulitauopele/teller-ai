1d4ae5df2ff47da7daa7750caed251c5db7c3037
Autumn-Martin <autumnmartin.dev@gmail.com>
2018-12-30 00:19:38 -0700

    Add param for most recent tweets to Twitter query

--- teller/views.py
+++ teller/views.py
@@ -6,7 +6,7 @@ import config
 
 def home(request):
     query = 'bitcoin'
-    url = f'https://api.twitter.com/1.1/search/tweets.json?q={query}&lang=en&count=100'
+    url = f'https://api.twitter.com/1.1/search/tweets.json?q={query}&lang=en&count=100&result_type=recent'
     headers = {'authorization': f'Bearer {config.twitter_token}'}
     response = requests.get(url, headers=headers)
     data = response.json()
