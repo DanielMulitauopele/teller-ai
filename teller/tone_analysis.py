d14b8078a025b75135c6e1f401491c4b1261f841
Autumn-Martin <autumnmartin.dev@gmail.com>
2018-12-31 07:30:45 -0700

    Add file to contain tone analysis methods

---
+++ teller/tone_analysis.py
@@ -0,0 +1,17 @@
+from django.shortcuts import render
+from django.http import JsonResponse
+from watson_developer_cloud import ToneAnalyzerV3
+import requests
+import config
+
+def analyze_tone_via_watson(document):
+    tone_analyzer = ToneAnalyzerV3(
+                        version='2017-09-21',
+                        iam_apikey=f'{config.watson_key}',
+                        url='https://gateway.watsonplatform.net/tone-analyzer/api'
+                        )
+    result = tone_analyzer.tone(
+                {'text': document},
+                'application/json',
+            ).get_result()
+    return result
