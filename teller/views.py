ea71dbb7e0efe3a891a14c00609513513f8065c6
Autumn-Martin <autumnmartin.dev@gmail.com>
2018-12-31 00:58:58 -0700

    Refactor - extract method to clean tweets for Watson analysis

--- teller/views.py
+++ teller/views.py
@@ -21,18 +21,7 @@ def retrieve_tweets(query):
         tweets.append(status_info['text'])
     return tweets
 
-def watson_analysis(request):
-    coin = request.GET.get('coin') # Either returns the query param value, or returns "None"
-    tweets = retrieve_tweets(coin)
-    # url = f'https://api.twitter.com/1.1/search/tweets.json?q={query}&lang=en&count=100&result_type=recent'
-    # headers = {'authorization': f'Bearer {config.twitter_token}'}
-    # response = requests.get(url, headers=headers)
-    # data = response.json()
-    # statuses = data['statuses']
-    # tweets = []
-    # for status_info in statuses:
-    #     tweets.append(status_info['text'])
-
+def clean_for_watson_analysis(tweets):
     cleaned_tweets = []
     for tweet in tweets:
     # CLEAN TWEET CONTRACTIONS
@@ -47,16 +36,38 @@ def watson_analysis(request):
             tweet = sentance.replace("#", "")
 
         cleaned_tweets.append(tweet)
+    return cleaned_tweets
+
+def watson_analysis(request):
+    coin = request.GET.get('coin') # Either returns the query param value, or returns "None"
+    tweets = retrieve_tweets(coin)
+    cleaned_tweets = clean_for_watson_analysis(tweets)
+    # cleaned_tweets = []
+    # for tweet in tweets:
+    # # CLEAN TWEET CONTRACTIONS
+    #     contractions = {"'s":"is", "'re":"are", "'ve":"have", "'nt":"not", "'d":"would", "'m":"am", "'ll":"will"}
+    #     poss_contractions = tweet.replace("'", " '").split(" ")
+    #     for poss_contraction in poss_contractions:
+    #         non_contraction = [contractions[poss_contraction] if poss_contraction in contractions else poss_contraction for poss_contraction in poss_contractions]
+    #
+    #         # CLEAN @TWITTERHANDLE & URLS
+    #         sentance = " ".join([word for word in non_contraction if 'http' not in word and '@' not in word])
+    #         # CLEAN HASHTAGS
+    #         tweet = sentance.replace("#", "")
+    #
+    #     cleaned_tweets.append(tweet)
     tweet_document = ' '.join(cleaned_tweets)
 
-    tone_analyzer = ToneAnalyzerV3(
-        version='2017-09-21',
-        iam_apikey=f'{config.watson_key}',
-        url='https://gateway.watsonplatform.net/tone-analyzer/api'
-    )
-
-    tone_analysis = tone_analyzer.tone(
-        {'text': tweet_document},
-        'application/json',
-    ).get_result()
-    return JsonResponse(tone_analysis, safe=False)
+    # tone_analyzer = ToneAnalyzerV3(
+    #     version='2017-09-21',
+    #     iam_apikey=f'{config.watson_key}',
+    #     url='https://gateway.watsonplatform.net/tone-analyzer/api'
+    # )
+    #
+    # tone_analysis = tone_analyzer.tone(
+    #     {'text': tweet_document},
+    #     'application/json',
+    # ).get_result()
+    #
+    # return JsonResponse(tone_analysis, safe=False)
+    return JsonResponse(cleaned_tweets, safe=False)
