f49c91dadb031f52321c0770778bc241a68f31f4
Autumn-Martin <autumnmartin.dev@gmail.com>
2018-12-30 23:34:14 -0700

    Add route for welcome page & map to its view

--- teller/urls.py
+++ teller/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.home, name='home'),
+    path('', views.WelcomePageView.as_view()),
+    path('watson_analysis', views.watson_analysis, name='watson_analysis'),
 ]
