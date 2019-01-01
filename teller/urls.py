17c8dd72efcc88f8a01f557c085f5699fa3785cb
Autumn-Martin <autumnmartin.dev@gmail.com>
2019-01-01 11:11:45 -0700

    Add name for welcome path for testing purposes

--- teller/urls.py
+++ teller/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.WelcomePageView.as_view()),
+    path('', views.WelcomePageView.as_view(), name='welcome'),
     path('watson_analysis', views.get_watson_analysis, name='watson_analysis'),
 ]
