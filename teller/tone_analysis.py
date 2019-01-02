85b36740feacac77e07f6660dd90cc2d1361498c
Autumn-Martin <autumnmartin.dev@gmail.com>
2019-01-02 15:10:32 -0700

    Using os.environ for env var storage

--- teller/tone_analysis.py
+++ teller/tone_analysis.py
@@ -1,14 +1,14 @@
+import os
 import requests
 from django.http import JsonResponse
 
 from watson_developer_cloud import ToneAnalyzerV3
 
-import config
 
 def analyze_tone_via_watson(document):
     tone_analyzer = ToneAnalyzerV3(
                         version='2017-09-21',
-                        iam_apikey=f'{config.watson_key}',
+                        iam_apikey=f'{os.environ.get("watson_key")}',
                         url='https://gateway.watsonplatform.net/tone-analyzer/api'
                         )
     result = tone_analyzer.tone(
--- teller/twitter_service.py
+++ teller/twitter_service.py
@@ -1,11 +1,11 @@
+import os
 import requests
 from django.http import JsonResponse
 
-import config
 
 def retrieve_twitter_data(query):
     url = f'https://api.twitter.com/1.1/search/tweets.json?q={query}&lang=en&count=100&result_type=recent'
-    headers = {'authorization': f'Bearer {config.twitter_token}'}
+    headers = {'authorization': f'Bearer {os.environ.get("twitter_token")}'}
     response = requests.get(url, headers=headers)
     data = response.json()
     return data['statuses']
