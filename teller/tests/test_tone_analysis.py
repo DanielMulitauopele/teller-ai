59b930c1b60ff27c8ad20b69734689e66d9cbeb2
Autumn-Martin <autumnmartin.dev@gmail.com>
2019-01-22 17:36:21 -0700

    Update test for watson analysis endpoint

--- teller/tests/test_tone_analysis.py
+++ teller/tests/test_tone_analysis.py
@@ -7,19 +7,5 @@ from .. import tone_analysis
 class AnalyzeToneViaWatsonTestCase(TestCase):
     def test_analyze_tone_via_watson_works(self):
         sentance = "Wow this totally works!"
-        expected_result = { "document_tone": {
-                                "tones": [
-                                  {
-                                    "score": 0.625141,
-                                    "tone_id": "joy",
-                                    "tone_name": "Joy"
-                                  },
-                                  {
-                                    "score": 0.97759,
-                                    "tone_id": "confident",
-                                    "tone_name": "Confident"
-                                  }
-                                ]
-                              }
-                          }
+        expected_result = { "document_tones": ["joy","confident"] }
         self.assertEqual(tone_analysis.analyze_tone_via_watson(sentance), expected_result)
